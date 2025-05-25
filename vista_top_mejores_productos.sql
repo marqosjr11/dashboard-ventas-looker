3a7601028027e85a1f0a6ccc5c61f1d9e0a6abbd
Marcos C <marqosjr1114@gmail.com>
2025-05-24 18:08:38 -0600

    Delete vista_top_mejores_productos.sql

--- vista_top_mejores_productos.sql
+++
@@ -1,7 +0,0 @@
-﻿SELECT ROW_NUMBER () OVER(ORDER BY Total_vendido DESC) AS Top,
-Description ,Total_vendido
-FROM (SELECT Description, SUM(Quantity) AS Total_vendido
-FROM `proyectomayo-460521.mayo25.filtrado`
-GROUP BY Description) AS sub
-ORDER BY Total_vendido  DESC
-LIMIT 10;
