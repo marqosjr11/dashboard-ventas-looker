d1c4b16256a71872f209fa7515a7cbf30c88b38c
Marcos C <marqosjr1114@gmail.com>
2025-05-24 18:08:10 -0600

    Delete vista_kpis_generales.sql

--- vista_kpis_generales.sql
+++
@@ -1,5 +0,0 @@
-﻿SELECT
-ROUND(SUM(Quantity * UnitPrice),2) AS Total,
-COUNT(DISTINCT CustomerID) AS Total_clientes,
-ROUND(SUM(Quantity*UnitPrice)/COUNT(DISTINCT InvoiceNo),2) AS Ticket_promedio
-from `proyectomayo-460521.mayo25.filtrado`
